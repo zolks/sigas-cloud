24727be6ff0bd60ff8183b7f0d814e813aa915b3
Marcelo Braga <marcelo.braga@sixbell.com>
2022-05-23 12:14:51 -0300

    initial commit

---
+++ src/main/java/com/sixbell/sigas/SigasController.java
@@ -0,0 +1,75 @@
+package com.sixbell.sigas;
+
+import io.opentracing.Tracer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class SigasController {
+
+    private static final String RESPONSE_STRING_FORMAT = "customer => %s\n";
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final RestTemplate restTemplate;
+
+    @Value("${preferences.api.url:http://rios:8080}")
+    private String remoteURL;
+
+    @Autowired
+    private Tracer tracer;
+
+    public SigasController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @RequestMapping(value = "/establish", method = RequestMethod.PUT, consumes = "application/json")
+    public ResponseEntity<String> receiveCall(@RequestBody String body) {
+        try {
+            return restTemplate.postForEntity(remoteURL+"/rios/establish", body, String.class);
+        } catch (HttpStatusCodeException ex) {
+            logger.warn("Exception trying to post to rios service.", ex);
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body(String.format("%d %s", ex.getRawStatusCode(), createHttpErrorResponseString(ex)));
+        } catch (RestClientException ex) {
+            logger.warn("Exception trying to post to rios service.", ex);
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body(ex.getMessage());
+        }
+    }
+
+    @RequestMapping(value = "/registerFlow", method = RequestMethod.PUT, consumes = "application/json")
+    public ResponseEntity<String> registerFlow(@RequestBody String body) {
+        try {
+            return restTemplate.postForEntity(remoteURL+"/rios/registerFlow", body, String.class);
+        } catch (HttpStatusCodeException ex) {
+            logger.warn("Exception trying to post to rios service.", ex);
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(String.format("%d %s", ex.getRawStatusCode(), createHttpErrorResponseString(ex)));
+        } catch (RestClientException ex) {
+            logger.warn("Exception trying to post to rios service.", ex);
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(ex.getMessage());
+        }
+    }
+
+    private String createHttpErrorResponseString(HttpStatusCodeException ex) {
+        String responseBody = ex.getResponseBodyAsString().trim();
+        if (responseBody.startsWith("null")) {
+            return ex.getStatusCode().getReasonPhrase();
+        }
+        return responseBody;
+    }
+
+}
---
+++ src/test/java/com/sixbell/sigas/SigasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sixbell.sigas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SigasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
